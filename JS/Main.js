83c42f136b9e9b8893eb9fd41020200e24943431
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-14 19:13:42 -0400

    CAPITALS REALLY

--- JS/Main.js
+++ JS/Main.js
@@ -1,4 +1,4 @@
-document.onload = function(){
+document.onLoad = function(){
   if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.marginTop = "32px";}
   if(typeof document.getElementById("BotFix") != "undefined"){document.body.style.marginBottom = "32px";}
 }
