85a12edc7876be16e3c760e51d2f3ac389826f18
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-14 12:24:05 -0400

    Create Main.js

---
+++ JS/Main.js
@@ -0,0 +1,2 @@
+if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.margin-top = "32px";}
+if(typeof document.getElementById("BotFix") != "undefind"){document.body.style.margin-bottom = "32px";}
