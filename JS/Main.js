d04869338884fc0cd37a8a57219b9c55b5cea0cd
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-14 12:53:01 -0400

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,2 +1,4 @@
-if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.margin-top = "32px";}
-if(typeof document.getElementById("BotFix") != "undefind"){document.body.style.margin-bottom = "32px";}
+window.onload = function(){
+  if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.margin-top = "32px";}
+  if(typeof document.getElementById("BotFix") != "undefind"){document.body.style.margin-bottom = "32px";}
+}
