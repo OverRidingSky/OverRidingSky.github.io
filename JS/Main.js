909e02467434093a67ebe383d08f0b6dfae6d2ac
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-15 19:31:03 -0400

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,7 +1,3 @@
-document.body.onLoad = pageSetup();
-function pageSetup(){
-	marginFix();
-}
 function marginFix(){
 	if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.marginTop = "32px";}
 	if(typeof document.getElementById("BotFix") != "undefined"){document.body.style.marginBottom = "32px";}
