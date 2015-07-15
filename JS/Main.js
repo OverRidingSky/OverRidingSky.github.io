8d94a38a5cbb17569c9fe37687c6cb91e20d74b2
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-15 13:58:00 -0400

    ues

--- JS/Main.js
+++ JS/Main.js
@@ -1,5 +1,5 @@
 pageSetup(){//intended to be used on page loaded.
-	marginFix();
+	setTimeout(function(){marginFix ()},25);
 }
 marginFix(){
 	if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.marginTop = "32px";}
