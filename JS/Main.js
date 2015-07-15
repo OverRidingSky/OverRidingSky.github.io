8869994be317187f226fd8913fe552fb70cbd96e
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-15 19:05:42 -0400

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,4 +1,8 @@
-marginFix(){
+document.onLoad = pageSetup();
+function pageSetup(){
+	marginFix();
+}
+function marginFix(){
 	if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.marginTop = "32px";}
 	if(typeof document.getElementById("BotFix") != "undefined"){document.body.style.marginBottom = "32px";}
 }
