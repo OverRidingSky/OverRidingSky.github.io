b0a7716c0239241a0e534464ce33470d9a1afc42
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-15 13:59:53 -0400

    not

--- JS/Main.js
+++ JS/Main.js
@@ -1,5 +1,5 @@
 pageSetup(){//intended to be used on page loaded.
-	setTimeout(function(){marginFix ()},25);
+	setTimeout(function(){marginFix ()},100);
 }
 marginFix(){
 	if(typeof document.getElementById("TopFix") != "undefined"){document.body.style.marginTop = "32px";}
