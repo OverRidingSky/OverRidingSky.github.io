f81a3a2b1600901f34c2a5e548f6c0f9c30d4a3b
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-25 00:23:40 -0400

    Update Internal.js

--- JS/Internal.js
+++ JS/Internal.js
@@ -15,15 +15,8 @@ function getScreenSize(){
 function fixHeight(){
 	h = getScreenSize()[1];
 	if(h > 1024){
-		hFix = h-1024;
-		var marTop, marBot;
-		while(hFix > 0){
-			marTop++;
-			hFix--;
-			marBot++;
-			hFix--;
-		}
-		document.getElementById('crossBorders').style.marginTop = marTop+"px";
+		hFix = ceil((h-1024)/2);
+		document.getElementById('crossBorders').style.marginTop = hFix + "px";
 	}
 }
 
