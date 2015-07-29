29dbc30e9c97e87ca26ebff3ae92963e2a65a7f4
OverRidingSky <OverRidingSky@users.noreply.github.com>
2015-07-29 11:41:47 -0400

    Update primarySets.js

--- JS/primarySets.js
+++ JS/primarySets.js
@@ -1,49 +1,47 @@
-"use strict";
-class cookie{
-  get = function(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
+function cookie(){return true;}
+function cookie.get(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
 }
 
-function check(name) {
-    var user = getCookie(name);
-    if (user != "") {
-        return true;
-    } else {
-        return false;
-    }
+function cookie.check(name) {
+  var user = getCookie(name);
+  if (user != "") {
+    return true;
+  }else{
+    return false;
+  }
 }
-function set(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+function cookie.set(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
 }
-function del(name){
+function cookie.del(name){
   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
 }
-class setSkin {
-  constructor(name){
-    if(name == null || name == ""){
-      console.log("You're doing it wrong. type 'setSkin.help();'");
-    }else{
-      list = ["map/maplvl1","map/maplvl2","map/maplvl3"];
-      for(i=0;i < list.length;i++){
-        if(cookie.check(list[i])){
+function setSkin(name){
+  if(name == null || name == ""){
+    console.log("You're doing it wrong. type 'setSkin.help();'");
+  }else{
+    list = ["map/maplvl1","map/maplvl2","map/maplvl3"];
+    for(i=0;i < list.length;i++){
+      if(cookie.check(list[i])){
         cookie.del(list[i]);
-        }
-        cookie.set(list[i], "http://overridingsky.github.io/Images/Skins/" + name + "/" + list[i] + ".jpg", 365);
       }
+      cookie.set(list[i], "http://overridingsky.github.io/Images/Skins/" + name + "/" + list[i] + ".jpg", 365);
     }
   }
-  function help(){
-  //log the help.
 }
+function setSkin.help(){
+//log the help.
 }
+
