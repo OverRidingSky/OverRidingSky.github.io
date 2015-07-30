c6fb4b8b5cd27cb2bfc35eabf35cc1b08c812238
OverRidingSky <SkyStarAntilux@gmail.com>
2015-07-30 14:08:55 -0400

    Data WIP
    WIP

---
+++ JS/mapGen0.1.js
@@ -0,0 +1,16 @@
+var Data = [];
+function Mapping(){
+  function loadData(){
+    var ca = document.cookie.split(';');
+    Dir = data[];
+    for(var i=0; i<ca.length; i++) {
+	  If(ca[i].split('/').length != 1){
+      if(){
+        
+      }
+	  }
+	}
+    return "";
+	
+  }
+}
--- JS/primarySets.js
+++ JS/primarySets.js
@@ -1,35 +1,39 @@
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0; i<ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1);
-    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-  }
-  return "";
-}
-function checkCookie(name) {
-  var user = getCookie(name);
-  if (user != "") {
-    return true;
-  }else{
-    return false;
-  }
-}
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-function delCookie(name){
-  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-}
 function setSkin(name){
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+  }
+  function checkCookie(name) {
+    var user = getCookie(name);
+    if (user != "") {
+      return true;
+    }else{
+      return false;
+    }
+  }
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+  }
+  function delCookie(name){
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  }
   if(name == null || name == ""){
     console.log("You're doing it wrong.");
   }else{
-    list = ["map/maplvl1","map/baseHouse"];
+	if(checkCookie("Meta")){
+      delCookie("Meta");
+    }
+    setCookie("Meta", "Example Meta Objects!", 365);
+    list = ["Map/maplvl1","Map/baseHouse"];
     for(i=0;i < list.length;i++){
       if(checkCookie(list[i])){
         delCookie(list[i]);
@@ -38,6 +42,6 @@ function setSkin(name){
     }
   }
 }
-document.onLoad = function(){
-  setSkin("Original0.1");
+if(checkCookie("Meta")){
+	setSkin("Original0.1");
 }
